6f1910f9322600816c9845b3d734ecf81c193eab
puttriMAX08 <putrifadilahsp08@gmail.com>
2026-09-16 20:26:58 +0700

    Add files via upload

---
+++ oleholeh.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array3D[3][3][4];
+    int angka = 2;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                array3D[i][j][k] = angka;
+                angka += 2;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Lapis Tabel " << i + 1 << ":" << endl;
+
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                cout << array3D[i][j][k] << "\t";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
